755e8f58a595b45a5841eeeea5da370df53f0608
ifs20001-itdel <ifs20001@students.del.ac.id>
2023-09-10 21:30:06 +0700

    Perbaikan

--- app/tugas.js
+++ app/tugas.js
@@ -1,41 +1,18 @@
-/*
-Nama	: Samuel Adika Lumbantobing
-Kampus	: IT Del
-Kelas	: FSWD (Kelas B)
-*/
-
-//========================================
-
 function getRandomNumber(excludedNumbers) {
     let randomNumber;
     do {
         randomNumber = Math.floor(Math.random() * 100) + 1;
-    } while (excludedNumbers.includes(randomNumber));
+    } while (excludedNumbers.has(randomNumber));
     return randomNumber;
 }
 
 function generateRandomNumbers(count) {
-    const randomNumbers = [];
-    while (randomNumbers.length < count) {
+    const randomNumbers = new Set();
+    while (randomNumbers.size < count) {
         const randomNumber = getRandomNumber(randomNumbers);
-        randomNumbers.push(randomNumber);
+        randomNumbers.add(randomNumber);
     }
-    return randomNumbers;
-}
-
-function splitEvenAndOddNumbers(numbers) {
-    const evenNumbers = [];
-    const oddNumbers = [];
-
-    for (const number of numbers) {
-        if (number % 2 === 0 && evenNumbers.length < 50) {
-            evenNumbers.push(number);
-        } else if (number % 2 !== 0 && oddNumbers.length < 50) {
-            oddNumbers.push(number);
-        }
-    }
-
-    return { evenNumbers, oddNumbers };
+    return Array.from(randomNumbers);
 }
 
 function calculateStatistics(numbers) {
@@ -58,68 +35,59 @@ function calculateStatistics(numbers) {
     return { min, max, total, average };
 }
 
+function splitEvenAndOddNumbersByIndex(numbers) {
+    const evenNumbers = [];
+    const oddNumbers = [];
+
+    for (let i = 0; i < numbers.length; i++) {
+        if (i % 2 === 0) {
+            evenNumbers.push(numbers[i]);
+        } else {
+            oddNumbers.push(numbers[i]);
+        }
+    }
+
+    return { evenNumbers, oddNumbers };
+}
+
 function compareStatistics(name1, statistics1, name2, statistics2) {
     console.log(`Perbandingan antara bilangan ${name1} dan ${name2}:`);
     console.log("===============================================");
-    console.log(`Minimum ${name1}:`, statistics1.min);
-    console.log(`Minimum ${name2}:`, statistics2.min);
 
-    if (statistics1.min < statistics2.min) {
-        console.log(`Minimum ${name1} lebih kecil dari Minimum ${name2}.`);
-    } else if (statistics1.min > statistics2.min) {
-        console.log(`Minimum ${name1} lebih besar dari Minimum ${name2}.`);
-    } else {
-        console.log(`Minimum ${name1} sama dengan Minimum ${name2}.`);
-    }
+    const compareValue = (valueName, value1, value2) => {
+        console.log(`${valueName} ${name1}:`, value1);
+        console.log(`${valueName} ${name2}:`, value2);
 
-    console.log(`\nMaksimum ${name1}:`, statistics1.max);
-    console.log(`Maksimum ${name2}:`, statistics2.max);
+        if (value1 < value2) {
+            console.log(`${valueName} ${name1} lebih kecil dari ${valueName} ${name2}.`);
+        } else if (value1 > value2) {
+            console.log(`${valueName} ${name1} lebih besar dari ${valueName} ${name2}.`);
+        } else {
+            console.log(`${valueName} ${name1} sama dengan ${valueName} ${name2}.`);
+        }
+    };
 
-    if (statistics1.max < statistics2.max) {
-        console.log(`Maksimum ${name1} lebih kecil dari Maksimum ${name2}.`);
-    } else if (statistics1.max > statistics2.max) {
-        console.log(`Maksimum ${name1} lebih besar dari Maksimum ${name2}.`);
-    } else {
-        console.log(`Maksimum ${name1} sama dengan Maksimum ${name2}.`);
-    }
-
-    console.log(`\nTotal ${name1}:`, statistics1.total);
-    console.log(`Total ${name2}:`, statistics2.total);
-
-    if (statistics1.total < statistics2.total) {
-        console.log(`Total ${name1} lebih kecil dari Total ${name2}.`);
-    } else if (statistics1.total > statistics2.total) {
-        console.log(`Total ${name1} lebih besar dari Total ${name2}.`);
-    } else {
-        console.log(`Total ${name1} sama dengan Total ${name2}.`);
-    }
-
-    console.log(`\nRata-rata ${name1}:`, statistics1.average);
-    console.log(`Rata-rata ${name2}:`, statistics2.average);
-
-    if (statistics1.average < statistics2.average) {
-        console.log(`Rata-rata ${name1} lebih kecil dari Rata-rata ${name2}.`);
-    } else if (statistics1.average > statistics2.average) {
-        console.log(`Rata-rata ${name1} lebih besar dari Rata-rata ${name2}.`);
-    } else {
-        console.log(`Rata-rata ${name1} sama dengan Rata-rata ${name2}.`);
-    }
+    compareValue("Minimum", statistics1.min, statistics2.min);
+    compareValue("Maksimum", statistics1.max, statistics2.max);
+    compareValue("Total", statistics1.total, statistics2.total);
+    compareValue("Rata-rata", statistics1.average, statistics2.average);
 }
 
 const randomNumbers = generateRandomNumbers(100);
-const { evenNumbers, oddNumbers } = splitEvenAndOddNumbers(randomNumbers);
-const evenStatistics = calculateStatistics(evenNumbers);
-const oddStatistics = calculateStatistics(oddNumbers);
+const { evenNumbers, oddNumbers } = splitEvenAndOddNumbersByIndex(randomNumbers);
 
 console.log("Tampilan 100 angka Random");
 console.log(randomNumbers);
 console.log("==================================================");
 console.log("BAGI MENJADI 2 ARRAY");
 console.log("==================================================");
-console.log("Tampilan 50 bilangan Genap");
+console.log("Tampilan 50 bilangan Genap (Berdasarkan Urutan Index)");
 console.log(evenNumbers);
-console.log("Tampilan 50 bilangan Ganjil");
+console.log("Tampilan 50 bilangan Ganjil (Berdasarkan Urutan Index)");
 console.log(oddNumbers);
 console.log("==================================================");
+
+const evenStatistics = calculateStatistics(evenNumbers);
+const oddStatistics = calculateStatistics(oddNumbers);
 
 compareStatistics("Ganjil", oddStatistics, "Genap", evenStatistics);
